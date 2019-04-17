9ceb6f72e59a16f8ea267b47fc0d8191bbc82f56
notintheup <brianjandron@gmail.com>
2019-04-17 17:15:37 -0400

    tweaks

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # bamazon
 An Amazon-like storefront with the MySQL skills you learned this unit. The app will take in orders from customers and deplete stock from the store's inventory. As a bonus task, you can program your app to track product sales across your store's departments and then provide a summary of the highest-grossing departments in the store.
+# Bamazon Customer View 
+* Running 'bamazonCustomer.js` will first display all of the items available for sale. This will include the ids, names, prices of products for sale and the stock quantity.
+
+* The app will then prompt users with two messages.
+
+   * The first will ask for the ID of the product that they would like to buy.
+   * The second message will ask how many units of the product they would like to buy.
+
+* Once the customer has placed the order, the application will check if the store has sufficient product items to meet the customer's request.
+
+   * If not, the app will log the phrase `Insufficient quantity!`, and prevent the order from going through.
+
+##VIDEO SUBMISSION https://drive.google.com/open?id=1_eumZIlZcV_OKgBO_RrAvcHYditm94SN
+#VIEW OF PRODUCT INVENTORY
+#USER INPUT SELECTION
+#RESULTS RETURNED EQUAL
+  #PRODUCT CHOSEN
+  #PRODUCT RESULTS
+  #TOTAL COST
+  #UPDATED INVENTORY
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,35 +1,88 @@
+//LOAD VARIABLES AND DEPENDENCIES
 var mysql = require("mysql");
-
-var mysql = require("mysql");
-
+var table = require("console.table");
+var inquirer = require("inquirer");
+//LOAD DATABASE CONNECTION
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "password",
   database: "bamazon_db"
 });
-
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  queryAllProducts();
-});
-
-function queryAllProducts() {
-  connection.query("SELECT * FROM products", function (err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(`---------------------------------------- 
-${res[i].item_id + " | "}${res[i].product_name+ " | "}${res[i].department_name+ " | "}${res[i].price+ " | "}${res[i].stock_quantity}
-----------------------------------------`)
-     
-    }
-    connection.end();
+//LIST PRODUCTS IN A TABLE
+function listProducts() {
+  connection.connect(function (err) {
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err
+      else console.table(res, "\n");
+      productId();
+    });
   });
+}
+listProducts();
+//USING INQUIRER: PROMPT FOR INPUT FROM USER
+function productId() {
+  inquirer.prompt([{
+      type: "input",
+      name: "id",
+      message: "Enter the product you would like to purchase:\n",
+      validate: function (value) {
+        if (!isNaN(value) && value < 11) {
+          return true;
+        }
+        return false;
+      }
+    },
+
+    {
+      type: "input",
+      name: "quant",
+      message: "Enter the quantity you would like to buy? \n",
+      validate: function (value) {
+        if (!isNaN(value)) {
+          return true;
+        }
+        return false;
+      }
+    }
+    //WITH USER INPUT, GRAB AND UPDATE THE DATABASE
+  ]).then(function (answer) {
+
+    var userId = answer.id;
+    console.log("Chosen item id: ", userId);
+    var userQuant = answer.quant;
+    console.log("Chosen quantity from stock: ", userQuant, "\n");
+    connection.query("SELECT * FROM products WHERE ?", [{
+      item_id: answer.id
+    }], function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      var current_quantity = res[0].stock_quantity;
+      console.log("IN-STOCK: ", current_quantity);
+      var price = res[0].price;
+      var remaining_quantity = current_quantity - answer.quant;
+      console.log("REMAINING STOCK: ", remaining_quantity);
+      if (current_quantity > answer.quant) {
+        console.log("Amount Remaining: " + remaining_quantity);
+        console.log("Total Cost: " + (answer.quant * price) + "\n");
+        connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?",
+          [
+            remaining_quantity, answer.id
+          ],
+          function (err, res) {
+            // console.table(res);
+          });
+        connection.query("SELECT * FROM products", function (err, res) {
+          console.log("INVENTORY UPDATED: ");
+          console.log("=============================================== \n");
+          console.table(res);
+        });
+      } else {
+        console.log("Insufficient amounts, please edit your units!");
+      }
+      connection.end();
+    });
+  })
+
 }
